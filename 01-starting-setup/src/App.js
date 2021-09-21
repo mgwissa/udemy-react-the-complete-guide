3e8d2be6510395010bc67b22cf77447e335bde1a
Michael Wissa <mwissa@uwm.com>
2021-09-21 10:40:29 -0400

    Please push

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -26,9 +26,9 @@ const App = () => {
   return (
     <div>
       <NewExpense></NewExpense>
-      <Expenses items={expenses}/>
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -4,9 +4,18 @@ import ExpenseForm from './ExpenseForm'
 import './NewExpense.css';
 
 const NewExpense = () => {
+
+  const SaveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    console.log(expenseData);
+  }
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={SaveExpenseDataHandler} />
     </div>
   );
 };
